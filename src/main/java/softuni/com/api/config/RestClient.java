c3668f972c8ec3512e8ed0e797b1c1f9f58fb516
mirelakalinova <119596034+mirelakalinova@users.noreply.github.com>
2025-12-01 21:21:17 +0200

    chore: initial commit + main configuraotins

---
+++ src/main/java/softuni/com/api/config/RestClient.java
@@ -0,0 +1,17 @@
+package softuni.com.api.config;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import softuni.com.api.app.Make.data.dto.ListMakesDto;
+import softuni.com.api.app.Model.data.dto.ListModelDto;
+
+
+@FeignClient(name = "apiClient", url = "${api.base}")
+public interface RestClient {
+	@GetMapping(value = "/GetMakesForVehicleType/car?format=json", produces = "application/json")
+	ListMakesDto getAllMakes();
+	@GetMapping(value = "/getmodelsformake/*?format=json", produces = "application/json")
+	ListModelDto getAllModels();
+	
+
+}
