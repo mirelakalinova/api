38876142a93c0892821eb47e7860b718fabc7cfd
mirelakalinova <119596034+mirelakalinova@users.noreply.github.com>
2025-12-01 21:26:08 +0200

    feat: added scheduler

---
+++ src/main/java/softuni/com/api/app/scheduler/updateMakesAndModels.java
@@ -0,0 +1,43 @@
+package softuni.com.api.app.scheduler;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import softuni.com.api.app.Make.service.MakeService;
+import softuni.com.api.app.Model.service.ModelService;
+
+@Component
+@Slf4j
+@Profile("!test")
+public class updateMakesAndModels {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(updateMakesAndModels.class);
+	private final MakeService makeService;
+	private final ModelService modelService;
+	
+	public updateMakesAndModels(MakeService makeService, ModelService modelService) {
+		this.makeService = makeService;
+		this.modelService = modelService;
+	}
+	
+	
+	@Scheduled(fixedDelayString = "${app.scheduling.update.delay}")
+	public void runUpdateJob() {
+		
+		
+		log.info("Starting update all makes job ..");
+		makeService.updateMakesData(makeService.fetchMakesData());
+		log.info("Starting update all models job ..");
+		modelService.updateModelsData(modelService.fetchModelsData());
+		
+		log.info("Successfully updated makes and models!");
+		
+	}
+	
+	
+}
