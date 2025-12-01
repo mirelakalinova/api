f805a1642662e2778135e4e0f3ba208c1ea94875
mirelakalinova <119596034+mirelakalinova@users.noreply.github.com>
2025-12-01 22:26:59 +0200

    chore: chore updates - allowed mehtods

--- src/main/java/softuni/com/api/config/WebConfig.java
+++ src/main/java/softuni/com/api/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 				.allowedOrigins(allowedOrigins)
-				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedMethods("GET", "POST")
 				.allowedHeaders("*");
 	}
 }
